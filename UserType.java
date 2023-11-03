97fe6945da18c2d432bbc33bc21a145b239db7e6
RSYR1906 <98223798+RSYR1906@users.noreply.github.com>
2023-11-03 21:41:51 +0800

    Create UserType.java

---
+++ UserType.java
@@ -0,0 +1,15 @@
+package main.model.user;
+
+/**
+ * This enum represents the type of the user
+ */
+public enum UserType {
+    /**
+     * The type of the user is student
+     */
+    STUDENT,
+    /**
+     * The type of the user is staff
+     */
+    STAFF,
+}

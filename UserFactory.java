55235a80ab85a9a67083b45f4acc705d697031f2
RSYR1906 <98223798+RSYR1906@users.noreply.github.com>
2023-11-03 21:50:17 +0800

    Update UserFactory.java

--- UserFactory.java
+++ UserFactory.java
@@ -9,14 +9,14 @@ import main.controller.account.password.PasswordHashManager;
 public class UserFactory {
     /**
      * Creates a new User object based on the given parameters.
-     *
+     * @param userID    The user's ID.
      * @param userType  The type of user to be created (student,staff).
      * @param password  The user's password in plaintext.
      * @param name      The user's name.
      * @param email     The user's email address.
      * @return          A new User object of the specified type.
      */
-    public static User create(UserType userType, String password, String name, String email) {
+    public static User create(UserType userType,String userID, String password, String name, String email) {
         String hashedPassword = PasswordHashManager.hashPassword(password);
         return switch (userType) {
             case STUDENT -> new Student(userID, name, email, hashedPassword);

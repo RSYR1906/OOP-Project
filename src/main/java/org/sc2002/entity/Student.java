35d8769bf55bdbbc50d3f8a77285f1b9b2a59e91
RSYR1906 <98223798+RSYR1906@users.noreply.github.com>
2023-11-06 11:08:49 +0800

    Update Student.java

--- src/main/java/org/sc2002/entity/Student.java
+++ src/main/java/org/sc2002/entity/Student.java
@@ -1,15 +1,59 @@
 package org.sc2002.entity;
 
-public class Student extends User{
+import java.util.ArrayList;
+import java.util.List;
+import org.sc2002.utils.exception.CampFullException;
+import org.sc2002.utils.exception.EntityNotFoundException;
 
+public class Student extends User {
 
+    private boolean isCampCommitteeMember;
+    private List<Camp> registeredCamps;
 
-    public Student(String name, String email,  String faculty) {
+    public Student(String name, String email, String faculty) {
         super(name, email, Faculty.valueOf(faculty.toUpperCase()));
+        this.isCampCommitteeMember = false;
+        this.registeredCamps = new ArrayList<>();
     }
 
     @Override
     public String getID() {
         return super.getEmail();
     }
+    
+    public boolean isCampCommitteeMember() {
+        return isCampCommitteeMember;
+    }
+
+    public void setCampCommitteeMember(boolean campCommitteeMember) {
+        isCampCommitteeMember = campCommitteeMember;
+    }
+
+    public void registerForCamp(Camp camp) {
+        if (!registeredCamps.contains(camp)) {
+            try {
+                camp.registerStudent(this);
+                registeredCamps.add(camp);
+            } catch (CampFullException e) {
+            	
+            }
+        }
+    }
+
+    public void withdrawFromCamp(Camp camp) {
+        if (registeredCamps.contains(camp)) {
+            try {
+                camp.withdrawStudent(this);
+                registeredCamps.remove(camp);
+            } catch (EntityNotFoundException e) {
+               
+            }
+        }
+    }
+
+
+    public List<Camp> getRegisteredCamps() {
+        return new ArrayList<>(registeredCamps);
+    }
 }
+

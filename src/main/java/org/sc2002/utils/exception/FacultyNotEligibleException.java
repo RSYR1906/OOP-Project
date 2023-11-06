d72435f7c6f23d2751f16560cd5c8cdb8c32798d
RSYR1906 <98223798+RSYR1906@users.noreply.github.com>
2023-11-06 11:13:08 +0800

    Create FacultyNotEligibleException.java

---
+++ src/main/java/org/sc2002/utils/exception/FacultyNotEligibleException.java
@@ -0,0 +1,9 @@
+package org.sc2002.utils.exception;
+
+public class FacultyNotEligibleException extends Exception {
+
+    public FacultyNotEligibleException(String message) {
+        super(message);
+    }
+
+}

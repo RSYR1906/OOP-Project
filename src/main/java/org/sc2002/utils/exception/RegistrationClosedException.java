d00177453748681a611bea6b4d06e5a74ea5d566
RSYR1906 <98223798+RSYR1906@users.noreply.github.com>
2023-11-06 11:13:44 +0800

    Create RegistrationClosedException.java

---
+++ src/main/java/org/sc2002/utils/exception/RegistrationClosedException.java
@@ -0,0 +1,9 @@
+package org.sc2002.utils.exception;
+
+public class RegistrationClosedException extends Exception {
+
+    public RegistrationClosedException(String message) {
+        super(message);
+    }
+
+}

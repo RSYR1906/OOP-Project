3dee74a0da0d934e3a5109d5428fe82572a4a4ac
RSYR1906 <98223798+RSYR1906@users.noreply.github.com>
2023-11-09 16:15:40 +0800

    Update Student.java

--- src/main/java/org/sc2002/entity/Student.java
+++ src/main/java/org/sc2002/entity/Student.java
@@ -1,5 +1,8 @@
 package org.sc2002.entity;
 
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 import org.sc2002.utils.exception.CampFullException;
@@ -17,8 +20,15 @@ public class Student extends User {
     }
 
     @Override
-    public String getID() {
-        return super.getEmail();
+ public String getID() {
+        Pattern pattern = Pattern.compile("^(.*?)@");
+        Matcher matcher = pattern.matcher(super.getEmail());
+        if (matcher.find()) {
+            // Group 1 contains the user ID
+            return matcher.group(1);
+        } else {
+            return null;
+        }
     }
     
     public boolean isCampCommitteeMember() {

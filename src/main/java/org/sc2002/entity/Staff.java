9f403533d70387408cc512a139f401a4d47560e0
RSYR1906 <98223798+RSYR1906@users.noreply.github.com>
2023-11-09 16:15:06 +0800

    Update Staff.java

--- src/main/java/org/sc2002/entity/Staff.java
+++ src/main/java/org/sc2002/entity/Staff.java
@@ -1,5 +1,8 @@
 package org.sc2002.entity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Staff extends User {
 
     public Staff(String name, String email, String password ,String faculty) {
@@ -8,6 +11,13 @@ public class Staff extends User {
 
     @Override
     public String getID() {
-        return super.getEmail();
+        Pattern pattern = Pattern.compile("^(.*?)@");
+        Matcher matcher = pattern.matcher(super.getEmail());
+        if (matcher.find()) {
+            // Group 1 contains the user ID
+            return matcher.group(1);
+        } else {
+            return null;
+        }
     }
 }

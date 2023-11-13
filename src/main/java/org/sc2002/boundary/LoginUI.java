ff47b53f688de65e36d58149638069b80df18b40
RSYR1906 <98223798+RSYR1906@users.noreply.github.com>
2023-11-13 19:18:08 +0800

    Update LoginUI.java

--- src/main/java/org/sc2002/boundary/LoginUI.java
+++ src/main/java/org/sc2002/boundary/LoginUI.java
@@ -3,38 +3,49 @@ package org.sc2002.boundary;
 import org.sc2002.controller.UserController;
 import org.sc2002.entity.User;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class LoginUI {
 
-    UserController userController;
+    private final UserController userController;
 
     public LoginUI(UserController userController) {
         this.userController = userController;
     }
 
-     public void login() {
+    public void login() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Login Page");
+
+        System.out.println("   _     _    ______    __        __        ______    __  ");
+        System.out.println("  | |   | |  |  ____|  |  |      |  |      |  __  |  |  |   ");
+        System.out.println("  | |___| |  |  |___   |  |      |  |      | |  | |  |  |   ");
+        System.out.println("  |  ___  |  |  ____|  |  |      |  |      | |  | |  |__|   ");
+        System.out.println("  | |   | |  |  |___   |  |___   |  |___   | |__| |   __    " );
+        System.out.println("  |_|   |_|  |______|  |______|  |______|  |______|  |__| ");
+
+        System.out.println("\u001B[36m\n =================================================================\u001B[0m");
+        System.out.println("\u001B[36m     Welcome to Camp Application and Management System (CAMs)    \u001B[0m");
+        System.out.println("\u001B[36m =================================================================\u001B[0m");
 
         // Get user input for user ID and password
-        System.out.print("Enter your user ID: ");
+        System.out.print("\u001B[33m\nEnter your user ID:\u001B[0m ");
         String userId = scanner.nextLine();
-        System.out.print("Enter your password: ");
+        System.out.print("\u001B[33mEnter your password:\u001B[0m ");
         String password = scanner.nextLine();
-
 
         User authenticatedUser = userController.authenticateUser(userId, password);
 
         if (authenticatedUser != null) {
-            System.out.println("Login successful. Welcome, " + authenticatedUser.getID() + "!");
-            System.out.println("Faculty: " + authenticatedUser.getFaculty());
-            System.out.println("User Role: " + userController.getUserRole(authenticatedUser));
+            System.out.println("\u001B[32m \nLogin successful. Welcome, " + authenticatedUser.getID() + "!\u001B[0m");
+            System.out.println("\u001B[34mFaculty:\u001B[0m " + authenticatedUser.getFaculty());
+            System.out.println("\u001B[34mUser Role:\u001B[0m " + userController.getUserRole(authenticatedUser));
         } else {
-            System.out.println("Login failed. Invalid user ID or password.");
+            System.out.println("\u001B[31m \nLogin failed. Invalid user ID or password.\u001B[0m");
         }
+
+        //System.out.println("\u001B[36m============================\u001B[0m");
 
         scanner.close();
     }
 }
+

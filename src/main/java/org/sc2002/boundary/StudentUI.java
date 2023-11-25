a201b48121c71e663919ad4a86b6307098e81b55
RSYR1906 <98223798+RSYR1906@users.noreply.github.com>
2023-11-25 19:05:24 +0800

    Update StudentUI.java

--- src/main/java/org/sc2002/boundary/StudentUI.java
+++ src/main/java/org/sc2002/boundary/StudentUI.java
@@ -1,168 +1,171 @@
-// StudentUI.java
 package org.sc2002.boundary;
 
-import org.sc2002.entity.User;
-import org.sc2002.entity.Camp;
-import org.sc2002.entity.Student;
+import org.sc2002.controller.CampController;
+import org.sc2002.controller.EnquiryController;
 import org.sc2002.controller.StudentController;
 import org.sc2002.controller.UserController;
+import org.sc2002.entity.Camp;
+import org.sc2002.entity.Staff;
+import org.sc2002.entity.Student;
+import org.sc2002.entity.User;
 import org.sc2002.repository.CampRepository;
+import org.sc2002.utils.exception.*;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
-public class StudentUI {
+public class StudentUI implements UI{
 
-    private final UserController userController;
-    private final StudentController studentController;
-    private final CampRepository campRepository;
+    private StudentController studentController;
+    private CampController campController;
+    private EnquiryController enquiryController;
 
-    public StudentUI(UserController userController, StudentController studentController, CampRepository campRepository) {
-        this.userController = userController;
+    private Student student;
+
+    public StudentUI(StudentController studentController, Student student, EnquiryController enquiryController, CampController campController) {
         this.studentController = studentController;
-        this.campRepository = campRepository;
+        this.student = student;
+        this.enquiryController = enquiryController;
+        this.campController = campController;
     }
 
-    public void displayStudentFeatures() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("\nChoose an option:");
-        System.out.println("1. Join Camps");
-        System.out.println("2. View Camps");
-        System.out.println("3. Withdraw from camp");
-        System.out.println("4. Write enquiries");
-        System.out.println("5. Exit");
-
-
-
-        System.out.print("\nYour choice (1-4): ");
-        int choice = scanner.nextInt();
-
-        switch (choice) {
-            case 1:
-                joinCamp();
-                break;
-            case 2:
-                viewCamps();
-                break;
-            case 3:
-                withdrawFromCamp();
-                break;
-            case 5:
-                System.out.println("\nExiting. Goodbye!");
-                break;
-            default:
-                System.out.println("\u001B[31mInvalid choice. Please enter a valid option.\u001B[0m");
-                break;
-        }
-
-        scanner.close();
+    public StudentUI(StudentController studentController2, CampController campController2,
+            UserController userController, CampRepository campRepository, Student user) {
     }
 
-    // Other methods like joinCamp() and viewCamps() go here
-     private void joinCamp() {
-        // Add logic to join a camp
+    @Override
+    public Object body() {
         Scanner scanner = new Scanner(System.in);
-        List<Camp> camps = campRepository.getAllCamps();
-    // Check if there are any camps available
-        if (camps.isEmpty()) {
-            System.out.println("\u001B[31mNo camps available to join.\u001B[0m");
-        }
 
-        // Display camps with an index
-        System.out.println("Available Camps:");
-        for (int i = 0; i < camps.size(); i++) {
-            Camp camp = camps.get(i);
-            System.out.println((i + 1) + ". " + camp.getCampName() + " - " + camp.getDescription());
-        }
+        System.out.println("\u001B[36m\nPlease enter your choice to continue:\u001B[0m");
+        while (true) {
+            System.out.println("1. View the list of camps along with the remaining available slots.");
+            System.out.println("2. Choose a camp and register for either as a camp attendee or camp committee");
+            System.out.println("3. Submit enquiries regarding a camp");
+            System.out.println("4. View your enquiries");
+            System.out.println("5. Edit your enquiries");
+            System.out.println("6. Delete your enquiries");
+            System.out.println("7. View the camps that you have already registered for");
+            System.out.println("8. Withdraw from camps that you have already registered for.");
+            System.out.println("9. Logout");
 
-        // Prompt the user to choose a camp
-        System.out.print("\nEnter the number of the camp you want to join: ");
-        int campIndex = scanner.nextInt();
-
-        // Validate the selected index
-        if (campIndex < 1 || campIndex > camps.size()) {
-            System.out.println("\u001B[31mInvalid selection. Please try again.\u001B[0m");
-        }
-
-        // Get the selected camp
-        Camp selectedCamp = camps.get(campIndex - 1);
-        Student student = (Student) userController.getCurrentUser();
-
-        try {
-            studentController.registerStudentToCamp(student, selectedCamp);
-            System.out.println("\u001B[32mJoin camp successfully.\u001B[0m");
-            System.out.println("\nWhat would you like to do next?");
-            System.out.println("1. Go back to the previous menu");
-            System.out.println("2. Exit the application");
-
-            System.out.print("\nYour choice (1-2): ");
+            System.out.print("\nYour choice (1-7): ");
             int choice = scanner.nextInt();
-
             switch (choice) {
                 case 1:
-                     displayStudentFeatures();  // Assuming this is the previous menu
-                     break;
-                case 2:
-                    System.out.println("\nExiting. Goodbye!");
-                    System.exit(0);
+                    viewCampsRemain();
                     break;
+                case 2:
+                    registerCamps();
+                    break;
+                case 3:
+                    //submitEnquiries();
+                    break;
+                case 4:
+                    //viewEnquiries();
+                    break;
+                case 5:
+                    //editEnquiries();
+                    break;
+                case 6:
+                    //deleteEnquery();
+                    break;
+                case 7:
+                    printRegisteredCamps();
+                    break;
+                case 8:
+                    withdrawFromCamp();
+                    break;
+                case 9:
+                    return logout(student);
                 default:
                     System.out.println("\u001B[31mInvalid choice. Please enter a valid option.\u001B[0m");
                     break;
- }        } catch (Exception e) {
-                System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
             }
-                
-                scanner.close();
+        }
+    }
+        
+    public boolean logout(User user) {
+        System.out.println("\u001B[33mYou have successfully logged out.\u001B[0m\n");
+        return true; // Indicate the user has logged out
+    }
+
+    Map<String, Integer> viewCampsRemain() {
+        Map<String, Integer> campSlotsMap = studentController.getCampRemainSlots(student);
+
+        // Use StringBuilder for efficient string concatenation
+        StringBuilder sb = new StringBuilder();
+
+        // Add header to the output
+        sb.append("\u001B[34mcamps, RemainSlots\u001B[0m\n");
+
+        // Iterate over the map and add each entry to the output
+        campSlotsMap.forEach((key, value) -> sb.append("\u001B[0m").append(key).append(", ").append(value).append("\n"));
+
+        // Print the result
+        System.out.println(sb.toString());
+
+        return campSlotsMap;
 
     }
 
-   private void viewCamps() {
-    Scanner scanner = new Scanner(System.in);
+    void registerCamps() {
+        Map<String, Integer> campSlotsMap = viewCampsRemain();
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        while (!campSlotsMap.containsKey(userInput)) {
+            System.out.println("Please enter the name of the camp you want to join");
+            userInput = scanner.nextLine();
+            if (!campSlotsMap.containsKey(userInput)) System.out.println("Please enter the correct camp name");
+        }
 
-    // Retrieve the list of camps within this method
-    List<Camp> camps = campRepository.getAllCamps();
 
-    // Display the header
-    System.out.println("\u001B[34m--- Available Camps ---\u001B[0m");
-    System.out.printf("%-5s %-20s %-15s %s\n", "No.", "Camp Name", "Dates", "Description");
+        System.out.println("Please enter whether to register as an attendee or a committee member: 0 member; 1 committee");
+        int input = scanner.nextInt();
+        try{
+            Camp camp = studentController.getCamp(userInput);
+            if (input == 0) {
+                studentController.registerCampAsStudent(student, camp);
+            } else if (input == 1) {
+                studentController.registerCampAsCampCommitteeMember(student, camp);
+            }
 
-    // Display each camp with an index
-    for (int i = 0; i < camps.size(); i++) {
-        Camp camp = camps.get(i);
-        String campInfo = String.format("%-5d %-20s %-15s %s",
-                (i + 1),
-                camp.getCampName(), // Assuming Camp has a method getCampName()
-                camp.getCampStartDate() + " to " + camp.getCampEndDate(), // Assuming Camp has methods getCampStartDate() and getCampEndDate()
-                camp.getDescription()); // Assuming Camp has a method getDescription()
-        System.out.println(campInfo);
+            System.out.println("Successfully registered for: " + camp.getCampName());
+
+
+
+        } catch (Exception e){
+            System.out.println("Failed to register: " + e.getMessage());
+        }
     }
 
-    // Rest of the method for user interaction
-    System.out.println("\nWhat would you like to do next?");
-    System.out.println("1. Go back to the previous menu");
-    System.out.println("2. Exit the application");
+    void withdrawFromCamp(){
+        ArrayList<Camp> registeredCamps = student.getRegisteredCamps();
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        while (true) {
+            System.out.println("Please enter the name of the camp you want to withdraw from");
+            userInput = scanner.nextLine();
+            String finaluserInput = userInput;
+            if (registeredCamps.stream().anyMatch(camp -> camp.getCampName().equals(finaluserInput))) {
+                break;
+            } else {
+                System.out.println("Please enter the correct camp name");
+            }
+        }
 
-    System.out.print("\nYour choice (1-2): ");
-    int choice = scanner.nextInt();
+        try{
+            Camp camp = studentController.getCamp(userInput);
+            studentController.withdrawFromCamp(student, camp);
+            System.out.println("Successfully withdrawn from: " + camp.getCampName());
 
-    switch (choice) {
-        case 1:
-            displayStudentFeatures();  // Assuming this is the previous menu
-            break;
-        case 2:
-            System.out.println("\nExiting. Goodbye!");
-            System.exit(0);
-            break;
-        default:
-            System.out.println("\u001B[31mInvalid choice. Please enter a valid option.\u001B[0m");
-            break;
+        } catch (Exception e){
+            System.out.println("Failed to Withdraw: " + e.getMessage());
+        }
     }
-    
-    scanner.close();
-}
-    private void withdrawFromCamp() {
 
-}
+    void printRegisteredCamps(){
+        studentController.printRegisteredCamps(student);
+    }
 }
